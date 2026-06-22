521662202fae703caf7d0689736534cfcb3be7fa
Jaime Arias <arias@lipn.univ-paris13.fr>
2026-06-22 13:11:11 +0200

    add full list of tests

--- tests/functional/todos.spec.ts
+++ tests/functional/todos.spec.ts
@@ -1,3 +1,4 @@
+import Todo from '#models/todo'
 import testUtils from '@adonisjs/core/services/test_utils'
 import { test } from '@japa/runner'
 
@@ -7,5 +8,28 @@ test.group('Todos', (group) => {
   test('list todos', async ({ client }) => {
     const res = await client.get('/todos')
     res.assertOk()
+  })
+
+  test('get todo', async ({ client }) => {
+    const todo = await Todo.create({ title: 'Buy groceries' })
+    const res = await client.get(`/todos/${todo.id}`)
+    res.assertOk()
+  })
+
+  test('create todo', async ({ client }) => {
+    const res = await client.post('/todos').json({ title: 'Buy groceries' })
+    res.assertCreated()
+  })
+
+  test('update todo', async ({ client }) => {
+    const todo = await Todo.create({ title: 'Buy groceries' })
+    const res = await client.put(`/todos/${todo.id}`).json({ title: 'Buy groceries and snacks' })
+    res.assertOk()
+  })
+
+  test('delete todo', async ({ client }) => {
+    const todo = await Todo.create({ title: 'Buy groceries' })
+    const res = await client.delete(`/todos/${todo.id}`)
+    res.assertNoContent()
   })
 })
