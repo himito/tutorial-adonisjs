e2e509ad5809763aac204fb99619184c629668fc
Jaime Arias <arias@lipn.univ-paris13.fr>
2026-06-23 08:17:33 +0200

    add test for the login and register

--- app/controllers/auth_controller.ts
+++ app/controllers/auth_controller.ts
@@ -1,41 +1,34 @@
 import User from '#models/user'
-import UserTransformer from '#transformers/user_transformer'
 import { loginValidator, signupValidator } from '#validators/user'
 import type { HttpContext } from '@adonisjs/core/http'
 
 export default class AuthController {
-  async login({ request, serialize }: HttpContext) {
+  async login({ request, response }: HttpContext) {
     const { email, password } = await request.validateUsing(loginValidator)
 
     const user = await User.verifyCredentials(email, password)
     const token = await User.accessTokens.create(user)
 
-    return serialize({
-      user: UserTransformer.transform(user),
-      token: token.value!.release(),
-    })
+    return response.ok({ token: token.value!.release() })
   }
 
-  async logout({ auth }: HttpContext) {
+  async logout({ auth, response }: HttpContext) {
     const user = auth.getUserOrFail()
     if (user.currentAccessToken) {
       await User.accessTokens.delete(user, user.currentAccessToken.identifier)
     }
 
-    return {
+    return response.ok({
       message: 'Logged out successfully',
-    }
+    })
   }
 
-  async register({ request, serialize }: HttpContext) {
+  async register({ request, response }: HttpContext) {
     const { fullName, email, password } = await request.validateUsing(signupValidator)
 
     const user = await User.create({ fullName, email, password })
     const token = await User.accessTokens.create(user)
 
-    return serialize({
-      user: UserTransformer.transform(user),
-      token: token.value!.release(),
-    })
+    return response.created({ user, token: token.value!.release() })
   }
 }
---
+++ tests/functional/auth.spec.ts
@@ -0,0 +1,43 @@
+import { UserFactory } from '#database/factories/user_factory'
+import testUtils from '@adonisjs/core/services/test_utils'
+import { test } from '@japa/runner'
+
+test.group('Authentication', (group) => {
+  group.each.setup(() => testUtils.db().truncate())
+  test('should register a new user and return a token', async ({ client, assert }) => {
+    const payload = {
+      fullName: 'John Doe',
+      email: 'john.doe@example.com',
+      password: 'password123',
+      passwordConfirmation: 'password123',
+    }
+
+    const response = await client.post('/register').json(payload)
+
+    response.assertCreated()
+    response.assertBodyContains({
+      user: {
+        fullName: payload.fullName,
+        email: payload.email,
+      },
+    })
+
+    const body = response.body()
+    assert.exists(body.token)
+    assert.isString(body.token)
+    assert.isNotEmpty(body.token)
+  })
+
+  test('login and return a token', async ({ client, assert }) => {
+    const password = 'password123'
+    const user = await UserFactory.merge({ password }).create()
+
+    const response = await client.post('/login').json({ email: user.email, password })
+
+    response.assertOk()
+
+    const body = response.body()
+    assert.exists(body.token)
+    assert.isNotEmpty(body.token)
+  }).pin()
+})
